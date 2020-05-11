9d4e1bf87f90dc494e85574a9940cb4747711b7a
Wolfgang Ramos <wolfgang@ramosarhuis.de>
2020-05-12 00:40:47 +0200

    revised keyboard layout ergodox-wramos

--- keyboards/ergodox_ez/keymaps/wramos/keymap.c
+++ keyboards/ergodox_ez/keymaps/wramos/keymap.c
@@ -16,6 +16,8 @@
 bool IS_SHIFT_LOCK_ON = false;
 uint32_t CTL_C_WHEN_TAPPED_CTL_ALT_WHEN_HELD_TIMER = 0;
 uint32_t CTL_DOT_WHEN_TAPPED_CTL_ALT_WHEN_HELD_TIMER = 0;
+uint32_t GRAVE_WHEN_TAPPED_CAPS_WHEN_HELD_TIMER = 0;
+uint32_t MINUS_WHEN_TAPPED_CAPS_WHEN_HELD_TIMER = 0;
 
 enum custom_keycodes {
     EPRM = SAFE_RANGE,
@@ -24,6 +26,8 @@ enum custom_keycodes {
     SHIFT_LOCK,
     ctl_c_when_tapped_ctl_alt_when_held,
     ctl_dot_when_tapped_ctl_alt_when_held,
+    grave_when_tapped_caps_when_held,
+    minus_when_tapped_caps_when_held,
 };
 
 /* Keymap */
@@ -34,18 +38,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   ESC  |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  MEH/= | L4/A | L1/S |SHFT/D|   F  |   G  |------|           |------|   H  |   J  |SHFT/K| L2/L | L3/; | MEH/'  |
- * |--------+------+------+------+------+------| LGUI |           | RGUI |------+------+------+------+------+--------|
- * |   `    |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   -    |
+ * |  NUBS  |   Q  |   W  |   E  |   R  |   T  | Page |           | ESC  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|  up  |           |      |------+------+------+------+------+--------|
+ * |Shift/= | L4/A | L1/S |S-C/D |S-A/F |MEH/G |------|           |------|MEH/H |S-A/J |S-C/K | L2/L | L3/; |Shift/' |
+ * |--------+------+------+------+------+------| Page |           | Shift|------+------+------+------+------+--------|
+ * | CAPS/` |   Z  |   X  |   C  |   V  |   B  | down |           | Lock |   N  |   M  |   ,  |   .  |   /  | CAPS/- |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | NUBS | CAPS |      |      | HOME |                                       |  END |      |      | CAPS |      |
+ *   |      | LGUI |      |      | HOME |                                       |  END |      |      | RGUI |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- * L1 = number layer (NUMB)               |      |      |       |      |      |
+ * L1 = number layer (NUMB)               |Insert| NUML |       |Print |Delete|
  * L2 = symbol layer (SYMB)        ,------|------|------|       |------+------+------.
- * L3 = mouse layer (SPECIAL_LEFT) |CTL/  |ALT/  |      |       |      |ALT/  |CTL/  |
+ * L3 = mouse layer (SPECIAL_LEFT) |CTL/  |ALT/  | CMDL |       |Pause |ALT/  |CTL/  |
  * L4 = command layer (..._RIGHT)  |ENTER |TAB   |------|       |------|BCKSPC|SPACE |
  *                                 |      |      |CA/C-.|       |CA/C-c|      |      |
  *                                 `--------------------'       `--------------------'
@@ -57,23 +61,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
-  KC_NO,              KC_F1,                  KC_F2,         KC_F3,             KC_F4, KC_F5, KC_F6,
-  KC_ESC,             KC_Q,                   KC_W,          KC_E,              KC_R,  KC_T,  KC_NO,
-  MT(MOD_MEH,KC_EQL), LT(SPECIAL_RIGHT,KC_A), LT(NUMB,KC_S), MT(MOD_LSFT,KC_D), KC_F,  KC_G,
-  KC_GRV,             KC_Z,                   KC_X,          KC_C,              KC_V,  KC_B,  KC_LGUI,
-  KC_NUBS,            NON_LOCKING_CAPS,       KC_NO,         KC_NO,             KC_HOME,
-                                                           KC_NO,               KC_NO,
-                                                                                KC_NO,
-                                      MT(MOD_LCTL,KC_ENT), MT(MOD_LALT,KC_TAB), ctl_dot_when_tapped_ctl_alt_when_held,
+  KC_NO,                            KC_F1,                  KC_F2,         KC_F3,                      KC_F4,                      KC_F5,            KC_F6,
+  KC_NUBS,                          KC_Q,                   KC_W,          KC_E,                       KC_R,                       KC_T,             KC_PGUP,
+  MT(MOD_LSFT,KC_EQL),              LT(SPECIAL_RIGHT,KC_A), LT(NUMB,KC_S), MT(MOD_LSFT|MOD_LCTL,KC_D), MT(MOD_LSFT|MOD_LALT,KC_F), MT(MOD_MEH,KC_G),
+  grave_when_tapped_caps_when_held, KC_Z,                   KC_X,          KC_C,                       KC_V,                       KC_B,             KC_PGDN,
+  KC_NO,                            KC_LGUI,                KC_NO,         KC_NO,                      KC_HOME,
+                                                           KC_INS,              TG(NUMB),
+                                                                                TG(SPECIAL_RIGHT),
+                                      MT(MOD_LCTL,KC_ENT), MT(MOD_LALT,KC_TAB), ctl_c_when_tapped_ctl_alt_when_held,
   // right hand
-  KC_F7,   KC_F8,             KC_F9,             KC_F10,            KC_F11,                KC_F12,                   KC_NO,
-  KC_NO,   KC_Y,              KC_U,              KC_I,              KC_O,                  KC_P,                     KC_BSLS,
-           KC_H,              KC_J,              MT(MOD_RSFT,KC_K), LT(SYMB,KC_L),         LT(SPECIAL_LEFT,KC_SCLN), MT(MOD_MEH,KC_QUOT),
-  KC_RGUI, KC_N,              KC_M,              KC_COMM,           KC_DOT,                KC_SLSH,                  KC_MINS,
-                              KC_END,            KC_NO,             KC_NO,                 NON_LOCKING_CAPS,         KC_NO,
-  SHIFT_LOCK,                          KC_NO,
-  KC_NO,
-  ctl_c_when_tapped_ctl_alt_when_held, MT(MOD_LALT,KC_BSPC), MT(MOD_RCTL,KC_SPC)
+  KC_F7,      KC_F8,            KC_F9,                      KC_F10,                     KC_F11,        KC_F12,                   KC_NO,
+  KC_ESC,     KC_Y,             KC_U,                       KC_I,                       KC_O,          KC_P,                     KC_BSLS,
+              MT(MOD_MEH,KC_H), MT(MOD_RSFT|MOD_LALT,KC_J), MT(MOD_RSFT|MOD_RCTL,KC_K), LT(SYMB,KC_L), LT(SPECIAL_LEFT,KC_SCLN), MT(MOD_RSFT,KC_QUOT),
+  SHIFT_LOCK, KC_N,             KC_M,                       KC_COMM,                    KC_DOT,        KC_SLSH,                  minus_when_tapped_caps_when_held,
+                                KC_END,                     KC_NO,                      KC_NO,         KC_RGUI,                  KC_NO,
+  KC_PSCR,                               KC_DEL,
+  KC_PAUS,
+  ctl_dot_when_tapped_ctl_alt_when_held, MT(MOD_LALT,KC_BSPC), MT(MOD_RCTL,KC_SPC)
 ),
 /* Keymap 1: Number Layer
  *
@@ -203,11 +207,11 @@ KC_TRNS, KC_ACL2, KC_ACL1
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      | PgUp |Delete|  UP  |Insert| Print|        |
+ * |         |      |      |      |      |      |      |           |      |      |      |  UP  |      |      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |------|           |------|PgDown| LEFT | DOWN | RIGHT| APP  |        |
+ * |         |      |      |      |      |      |------|           |------|      | LEFT | DOWN | RIGHT| APP  |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |      |           |      |      | PAUSE|      | EPRN | VRSN |        |
+ * |         |      |      |      |      |      |      |           |      |      |      |      | EPRN | VRSN |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |       |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -231,9 +235,9 @@ KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS,
 // TODO: right hand
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-KC_TRNS, KC_PGUP, KC_DEL,  KC_UP,   KC_INS,  KC_PSCR, KC_TRNS,
-         KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_APP,  KC_TRNS,
-KC_TRNS, KC_TRNS, KC_PAUS, KC_TRNS, EPRM,    VRSN,    KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,
+         KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_APP,  KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EPRM,    VRSN,    KC_TRNS,
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS,
 KC_TRNS,
@@ -304,6 +308,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
         }
         return false;
+    case grave_when_tapped_caps_when_held
+        if(record->event.pressed) {
+            GRAVE_WHEN_TAPPED_CAPS_WHEN_HELD_TIMER = timer_read32();
+            tap_code(KC_CAPS);
+        } else {
+            uint32_t elapsed = timer_elapsed32(CTL_DOT_WHEN_TAPPED_CTL_ALT_WHEN_HELD_TIMER);
+            if(elapsed <= TAPPING_TERM)
+            {
+                tap_code(KC_CAPS);
+                tap_code(KC_GRV);
+            } else {
+                tap_code(KC_CAPS);
+            }
+        }
+        return false;
+    case minus_when_tapped_caps_when_held
+        if(record->event.pressed) {
+            MINUS_WHEN_TAPPED_CAPS_WHEN_HELD_TIMER = timer_read32();
+            tap_code(KC_CAPS);
+        } else {
+            uint32_t elapsed = timer_elapsed32(CTL_DOT_WHEN_TAPPED_CTL_ALT_WHEN_HELD_TIMER);
+            if(elapsed <= TAPPING_TERM)
+            {
+                tap_code(KC_CAPS);
+                tap_code(KC_MINS);
+            } else {
+                tap_code(KC_CAPS);
+            }
+        }
+    return false;
     default:
         return true;
     }
