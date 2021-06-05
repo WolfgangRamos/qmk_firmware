3b2578cc115198fd023ee22f5911503ff01f725b
Wolfgang Ramos <wolfgang@ramos.arhuis.de>
2021-06-05 21:12:01 +0200

    Removed shifit-dual function keys

--- keyboards/ergodox_ez/keymaps/wramos/keymap.c
+++ keyboards/ergodox_ez/keymaps/wramos/keymap.c
@@ -78,34 +78,6 @@ bool process_tap_hold(uint16_t keycode, keyrecord_t *record) {
 
 
 /* tap hold configuration */
-bool shift_tab_ctrl_alt_key_down_release_alt_on_key_up = true;
-
-void shift_tab_ctrl_alt_key_down(keyrecord_t *record)
-{
-    if(get_mods() & (TAP_HOLD_LALT | TAP_HOLD_RALT)) {
-        shift_tab_ctrl_alt_key_down_release_alt_on_key_up = false;
-        register_mods(TAP_HOLD_RCTL);
-    } else {
-        shift_tab_ctrl_alt_key_down_release_alt_on_key_up = true;
-        register_mods(TAP_HOLD_RCTL | TAP_HOLD_RALT);
-    }
-}
-
-void shift_tab_ctrl_alt_key_up(keyrecord_t *record, uint16_t elapsed)
-{
-    if(shift_tab_ctrl_alt_key_down_release_alt_on_key_up) {
-        unregister_mods(TAP_HOLD_RCTL | TAP_HOLD_RALT);
-    } else {
-        unregister_mods(TAP_HOLD_RCTL);
-    }
-
-    if(elapsed <= TAPPING_TERM) {
-        register_mods(TAP_HOLD_LSFT);
-        tap_code(KC_TAB);
-        unregister_mods(TAP_HOLD_LSFT);
-    }
-}
-
 void shift_d_lshift_key_down(keyrecord_t *record)
 {
     register_mods(TAP_HOLD_LSFT);
@@ -178,8 +150,7 @@ void shift_j_arrow_layer_key_up(keyrecord_t *record, uint16_t elapsed)
 }
 
 enum tap_hold_enum {
-    SHIFT_TAB_CTRL_ALT = 0,
-    SHIFT_S_NUMBER_LAYER,
+    SHIFT_S_NUMBER_LAYER = 0,
     SHIFT_L_SYMBOL_LAYER,
     SHIFT_J_ARROW_LAYER,
     SHIFT_K_LSHIFT,
@@ -187,7 +158,6 @@ enum tap_hold_enum {
 };
 
 tap_hold_t tap_holds[] = {
-    [SHIFT_TAB_CTRL_ALT] = TAP_HOLD(shift_tab_ctrl_alt_key_down, shift_tab_ctrl_alt_key_up),
     [SHIFT_D_LSHIFT] = TAP_HOLD(shift_d_lshift_key_down, shift_d_lshift_key_up),
     [SHIFT_K_LSHIFT] = TAP_HOLD(shift_k_lshift_key_down, shift_k_lshift_key_up),
     [SHIFT_S_NUMBER_LAYER] = TAP_HOLD(shift_s_number_layer_key_down, shift_s_number_layer_key_up),
@@ -197,19 +167,70 @@ tap_hold_t tap_holds[] = {
 
 
 enum custom_keycodes {
-    CAPS_A = SAFE_RANGE,
-    CAPS_E,
-    CAPS_S,
-    CAPS_O,
-    CAPS_U,
-    CAPS_SLSH,
-    CAPS_BSLS,
-    CAPS_NUBS,
-    CAPS_GRV,
-    CAPS_M,
-    CAPS_T,
-    CAPS_D,
+    CAPS_A = SAFE_RANGE, // german umlaut ae
+    CAPS_E, // e with trema
+    CAPS_S, // german umlaut sz
+    CAPS_O, // german umlaut oe
+    CAPS_U, // german umlaut ue
+    CAPS_SLSH, // euro sign
+    CAPS_BSLS, // paragraph sign
+    CAPS_NUBS, // unshifted: dead key accent acute; shifted: accent circonflex
+    CAPS_GRV, // unshifted: backtick; shifted: dead key accent grave
+    CAPS_M, // greek letter mu
+    CAPS_T, // tilde
+    CAPS_D, // degree sign
+    INV_QUOTE, // unshifted: double quote; shifted: single quote
+    INV_MINUS, // unshifted: underscore; shifted: minus
 };
+
+/* Inverting shift on keys*/
+void press_key_with_level_mods(uint16_t key) {
+    const uint8_t interesting_mods = MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT) | MOD_BIT(KC_RALT);
+
+    // Save the state
+    const uint8_t real_mods = get_mods();
+    const uint8_t weak_mods = get_weak_mods();
+    const uint8_t macro_mods = get_macro_mods();
+
+    uint8_t target_mods = (key >> 8) & (QK_MODS_MAX >> 8);
+    // The 5th bit indicates that it's a right hand mod,
+    // which needs some fixup
+    if (target_mods & 0x10) {
+        target_mods &= 0xF;
+        target_mods <<= 4;
+    }
+
+    // Clear the mods that we are potentially going to modify,
+    del_mods(interesting_mods);
+    del_weak_mods(interesting_mods);
+    del_macro_mods(interesting_mods);
+
+    // Enable the mods that we need
+    add_mods(target_mods & interesting_mods);
+
+    // Press and release the key
+    register_code(key & 0xFF);
+    unregister_code(key & 0xFF);
+
+    // Restore the previous state
+    set_mods(real_mods);
+    set_weak_mods(weak_mods);
+    set_macro_mods(macro_mods);
+    send_keyboard_report();
+}
+
+void override_key(keyrecord_t* record, uint16_t normal, uint16_t shifted) {
+    const uint8_t shift = MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT);
+    if (record->event.pressed) {
+        bool shift_pressed = keyboard_report->mods & shift;
+        const uint16_t target = shift_pressed ? shifted : normal;
+        uint8_t keycode = target & 0xFF;
+        if (keycode == KC_NO) {
+            return;
+        }
+        press_key_with_level_mods(target);
+    }
+}
 
 /* Keymap */
 
@@ -218,41 +239,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(
 // left hand
 KC_NO,   KC_F1, KC_F2,         KC_F3,             KC_F4,   KC_F5, KC_F6,
-KC_NUBS, KC_Q,  KC_W,          KC_E,              KC_R,    KC_T,  LSFT(LALT(KC_S)),
-KC_EQL,  KC_A,  LT(NUMB,KC_S), MT(MOD_LSFT,KC_D), KC_F,    KC_G,
+KC_NUBS, KC_Q,  KC_W,          KC_E,              KC_R,    KC_T,  KC_LGUI,
+KC_MINS, KC_A,  LT(NUMB,KC_S), KC_LSFT,           KC_F,    KC_G,
 KC_GRV,  KC_Y,  KC_X,          KC_C,              KC_V,    KC_B,  KC_INS,
-KC_NO,   KC_NO, KC_NO,         KC_NO,             KC_LGUI,
+KC_NO,   KC_NO, KC_NO,         KC_NO,             MT(MOD_LCTL|MOD_LALT,KC_EQL),
 _NOKEY               KC_PAUSE,            KC_NO,
 _NOKEY               _NOKEY               TG(ARRW),
-MT(MOD_LCTL,KC_ENT), MT(MOD_LALT,KC_TAB), TH(SHIFT_TAB_CTRL_ALT),
+MT(MOD_LCTL,KC_ENT), MT(MOD_LALT,KC_TAB), LSFT(KC_TAB),
 
 // right hand
-KC_F7,    KC_F8,  KC_F9,         KC_F10,            KC_F11,        KC_F12,  EEPROM_RESET,
-KC_ESC,   KC_Z,   KC_U,          KC_I,              KC_O,          KC_P,    KC_BSLS,
-_NOKEY    KC_H,   LT(ARRW,KC_J), MT(MOD_LSFT,KC_K), LT(SYMB,KC_L), KC_SCLN, KC_QUOT,
-TG(SHFT), KC_N,   KC_M,          KC_COMM,           KC_DOT,        KC_SLSH, KC_MINS,
-_NOKEY    _NOKEY  KC_LGUI,       KC_NO,             KC_NO,         KC_NO,   KC_NO,
+KC_F7,    KC_F8,  KC_F9,                            KC_F10,  KC_F11,        KC_F12,  EEPROM_RESET,
+KC_ESC,   KC_Z,   KC_U,                             KC_I,    KC_O,          KC_P,    KC_BSLS,
+_NOKEY    KC_H,   LT(ARRW,KC_J),                    KC_LSFT, LT(SYMB,KC_L), KC_K,    INV_QUOTE,
+TG(SHFT), KC_N,   KC_M,                             KC_COMM, KC_DOT,        KC_D,  KC_SLSH,
+_NOKEY    _NOKEY  MT(MOD_LCTL|MOD_LALT,KC_SCLN),    KC_NO,   KC_NO,         KC_NO,   KC_NO,
 KC_NO,                        KC_PSCR,
 TG(NUMB),
-MT(MOD_LCTL|MOD_LALT,KC_DEL), MT(MOD_LALT,KC_BSPC), MT(MOD_LCTL,KC_SPC)
+KC_DEL, MT(MOD_LALT,KC_BSPC), MT(MOD_LCTL,KC_SPC)
 ),
 
 [SHFT] = LAYOUT_ergodox(
 // left hand
-KC_TRNS, KC_TRNS,    KC_TRNS,                  KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_TRNS,
-KC_TRNS, RSFT(KC_Q), RSFT(KC_W),               RSFT(KC_E),         RSFT(KC_R), RSFT(KC_T), KC_TRNS,
-KC_TRNS, RSFT(KC_A), TH(SHIFT_S_NUMBER_LAYER), TH(SHIFT_D_LSHIFT), RSFT(KC_F), RSFT(KC_G),
-KC_TRNS, RSFT(KC_Y), RSFT(KC_X),               RSFT(KC_C),         RSFT(KC_V), RSFT(KC_B), KC_TRNS,
-KC_TRNS, KC_TRNS,    KC_TRNS,                  KC_TRNS,            KC_TRNS,
+KC_TRNS,   KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+KC_TRNS,   RSFT(KC_Q), RSFT(KC_W),               RSFT(KC_E), RSFT(KC_R), RSFT(KC_T), KC_TRNS,
+INV_MINUS, RSFT(KC_A), TH(SHIFT_S_NUMBER_LAYER), KC_LSFT,    RSFT(KC_F), RSFT(KC_G),
+KC_TRNS,   RSFT(KC_Y), RSFT(KC_X),               RSFT(KC_C), RSFT(KC_V), RSFT(KC_B), KC_TRNS,
+KC_TRNS,   KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,
 _NOKEY   KC_TRNS,    KC_TRNS,
 _NOKEY   _NOKEY      KC_TRNS,
 KC_TRNS, KC_TRNS,    KC_TRNS,
 // right hand
-KC_TRNS, KC_TRNS,    KC_TRNS,                 KC_TRNS,            KC_TRNS,                  KC_TRNS,    KC_TRNS,
-KC_TRNS, RSFT(KC_Z), RSFT(KC_U),              RSFT(KC_I),         RSFT(KC_O),               RSFT(KC_P), KC_TRNS,
-_NOKEY   RSFT(KC_H), TH(SHIFT_J_ARROW_LAYER), TH(SHIFT_K_LSHIFT), TH(SHIFT_L_SYMBOL_LAYER), KC_TRNS,    KC_TRNS,
-KC_TRNS, RSFT(KC_N), RSFT(KC_M),              KC_TRNS,            KC_TRNS,                  KC_UNDS,    KC_TRNS,
-_NOKEY   _NOKEY      KC_TRNS,                 KC_TRNS,            KC_TRNS,                  KC_TRNS,    KC_TRNS,
+KC_TRNS, KC_TRNS,    KC_TRNS,                 KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,
+KC_TRNS, RSFT(KC_Z), RSFT(KC_U),              RSFT(KC_I), RSFT(KC_O),               RSFT(KC_P), KC_TRNS,
+_NOKEY   RSFT(KC_H), TH(SHIFT_J_ARROW_LAYER), KC_LSFT,    TH(SHIFT_L_SYMBOL_LAYER), RSFT(KC_K), KC_TRNS,
+KC_TRNS, RSFT(KC_N), RSFT(KC_M),              KC_TRNS,    KC_TRNS,                  RSFT(KC_D), KC_TRNS,
+_NOKEY   _NOKEY      KC_TRNS,                 KC_TRNS,    KC_TRNS,                  KC_TRNS,    KC_TRNS,
 KC_TRNS, KC_TRNS,
 KC_TRNS,
 KC_TRNS, KC_TRNS,    KC_TRNS
@@ -260,7 +281,7 @@ KC_TRNS, KC_TRNS,    KC_TRNS
 
 [NUMB] = LAYOUT_ergodox(
 // left hand
-KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
+KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -283,9 +304,9 @@ KC_TRNS, KC_TRNS, KC_TRNS
 [SYMB] = LAYOUT_ergodox(
 // left hand
 KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
-KC_TRNS, KC_AT,   KC_ASTR, KC_LCBR, KC_RCBR, KC_DLR,  KC_TRNS,
-KC_TRNS, KC_EXLM, KC_AMPR, KC_LPRN, KC_RPRN, KC_HASH,
-KC_TRNS, CAPS_T,  KC_CIRC, KC_LBRC, KC_RBRC, KC_PERC, KC_TRNS,
+KC_TRNS, KC_CIRC, KC_ASTR, KC_LCBR, KC_RCBR, KC_HASH,  KC_TRNS,
+KC_TRNS, KC_EXLM, KC_AMPR, KC_LPRN, KC_RPRN, KC_DLR,
+KC_TRNS, CAPS_T,  KC_AT,   KC_LBRC, KC_RBRC, KC_PERC, KC_TRNS,
 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 _NOKEY   KC_TRNS, KC_TRNS,
 _NOKEY   _NOKEY   KC_TRNS,
@@ -304,9 +325,9 @@ KC_TRNS, KC_TRNS, KC_TRNS
 [ARRW] = LAYOUT_ergodox(
 // left hand
 CAPS_NUBS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,
-KC_TRNS,   KC_TRNS,   KC_HOME, KC_UP,   KC_END,    KC_PGUP, KC_TRNS,
+KC_TRNS,   CAPS_M,    KC_HOME, KC_UP,   KC_END,    KC_PGUP, LSFT(LALT(KC_S)),
 KC_TRNS,   KC_APP,    KC_LEFT, KC_DOWN, KC_RGHT,   KC_PGDN,
-KC_TRNS,   CAPS_SLSH, CAPS_D,  CAPS_M,  CAPS_BSLS, KC_TRNS, KC_TRNS,
+KC_TRNS,   CAPS_SLSH, CAPS_D,  ,  KC_GT,     CAPS_BSLS, KC_TRNS,
 CAPS_GRV,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS,
 _NOKEY   KC_TRNS, KC_TRNS,
 _NOKEY   _NOKEY   KC_TRNS,
@@ -372,6 +393,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     case CAPS_D:
         caps(KC_D, record);
+        return false;
+    case INV_QUOTE:
+        override_key(record, LSFT(KC_QUOT), KC_QUOT);
+        return false;
+    case INV_MINUS:
+        override_key(record, LSFT(KC_MINS), KC_MINS);
         return false;
     default:
         return true;
